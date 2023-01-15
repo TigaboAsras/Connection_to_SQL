86eafc5d6ae23aea31cfdd1d25b0d027b9b13e9f
neoray16 <neoray3355@gmail.com>
2023-01-15 16:31:54 +0200

    add partner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("SHKSUKA");
-        System.out.println("trivago")
+        System.out.println("trivago");
+        System.out.println("my partner is neoray");
+
     }
 }
