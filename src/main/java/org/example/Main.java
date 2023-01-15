cf16b5808992a8a972b2da5ce723b81367dc9e64
Tigabo <asrastigabo@gmail.com>
2023-01-15 15:43:01 +0200

    my first commit on TestGit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+/**
+* @author :Tigabo,ID:329011274
+**/
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
